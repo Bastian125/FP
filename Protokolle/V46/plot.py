14de79913fbcf1a1c0b41c86e0531e7ae0f0bab2
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2023-04-17 16:44:08 +0200

    Plotzwischenstand

--- Protokolle/V46/plot.py
+++ Protokolle/V46/plot.py
@@ -27,4 +27,28 @@ d2 = (a12 + g12/60)*(np.pi)/180
 ld1 = np.abs(d1-d2)
 
 # als txt speichern
-np.savetxt('winkeldiff1.txt', ld1)
+np.savetxt('winkeldiff1', ld1)
+
+# dotiert1 Auswertung
+l2, a21, g21, a22, g22 = np.genfromtxt('data/dotiert1.txt', unpack=True)
+# Daten von Grad+Winkelminuten in Radiant umrechnen
+d3 = (a21 + g21/60)*(np.pi)/180
+d4 = (a22 + g22/60)*(np.pi)/180
+
+# Differenz berechnen
+ld2 = np.abs(d3-d4)
+
+# als txt speichern
+np.savetxt('winkeldiff2.txt', ld2)
+
+# dotiert2 Auswertung
+l3, a31, g31, a32, g32 = np.genfromtxt('data/dotiert2.txt', unpack=True)
+# Daten von Grad+Winkelminuten in Radiant umrechnen
+d5 = (a31 + g31/60)*(np.pi)/180
+d6 = (a32 + g32/60)*(np.pi)/180
+
+# Differenz berechnen
+ld3 = np.abs(d5-d6)
+
+# als txt speichern
+np.savetxt('winkeldiff3.txt', ld3)
